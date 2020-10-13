1f3e446fd6c03f6eb6b42054583e43ea761fdb67
Juanjo Perez <jjperez85@gmail.com>
2020-10-13 16:58:47 -0400

    Edit site loader layout and styling

--- public/templates/site-loader.php
+++ public/templates/site-loader.php
@@ -1,8 +1,5 @@
 <div id="overlay" class="overlay">
     <div id="loader" class="loader">
-        <div class="loader-logo">
-            <h1 class="loader-title"><?php bloginfo('name'); ?></h1>
-        </div>
         <div class="spinner" role="status"></div>
     </div>
 </div>
