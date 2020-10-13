39f0c28092aea901fe8f857846582cdc95154f05
Juanjo Perez <jjperez85@gmail.com>
2020-10-13 17:48:58 -0400

    Display blog page content below title

--- public/templates/blog/blog-header.php
+++ public/templates/blog/blog-header.php
@@ -1,3 +1,11 @@
+<?php
+$blog_id = get_option('page_for_posts');
+$blog = get_post($blog_id);
+$blog_content = $blog->post_content;
+?>
 <header class="page-header blog-header">
     <h1 class="page-title blog-title"><?php echo get_the_title(get_option('page_for_posts')); ?></h1>
+    <?php if (is_home() && !empty($blog_content)) { ?>
+    <div class="page-content blog-content"><?php echo $blog_content; ?></div>
+    <?php } ?>
 </header>
